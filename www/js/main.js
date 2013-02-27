3c009942b12d9567e571e35c8711163a0c9bbf78
Gregor Martynus <gregor@martynus.net>
2013-02-27 15:05:46 +0100

    ace!

--- www/js/main.js
+++ www/js/main.js
@@ -1,26 +1,31 @@
 window.hoodie  = new Hoodie()
 
 $( function() {
+  var editor = ace.edit("editor");
+  // editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/html");
+  editor.setValue( 'wtf?' )
+
   var $preview = $('#preview')
-  var $code = $('#code textarea')
 
   var magic = hoodie.store.find('example', 'default')
   magic.done( function(object) {
     $preview.html( object.code )
-    $code.val( object.code )
+    editor.setValue( object.code )
   })
   magic.fail( function() {
     var code = $('#preview').html()
+
+    // clean up code a bit
     code = code.replace(/(^[^\n+]*\n|\n[^\n+]*$)/g, '');
     code = code.replace(/[ ]{12}/g, '');
-    window.code = code
-    console.log('code', code)
+
     hoodie.store.save('example', 'default', {code: code})
-    $code.val( code )
+    editor.setValue( code )
   })
 
-  $code.on('change', function() {
-    var code = $code.val()
+  editor.getSession().on('change', function() {
+    var code = editor.getValue()
     hoodie.store.update('example', 'default', {code: code})
     $preview.html( code )
   })
