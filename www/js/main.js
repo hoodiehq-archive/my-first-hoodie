142a0b425010e971fcbb9258e710b468a7963cfb
Gregor Martynus <gregor@martynus.net>
2013-02-27 11:42:13 +0100

    simplification of my first hoodie app

--- www/js/main.js
+++ www/js/main.js
@@ -1,15 +1,1 @@
 hoodie  = new Hoodie()
-
-$hoodieAccountModal = $('#hoodieAccountModal')
-.on('shown', function() {
-  $hoodieAccountModal.find('input').eq(0).focus()
-})
-.on('hide', function() {
-  $hoodieAccountModal.find('.alert').remove()
-  $hoodieAccountModal.find('input').val('')
-})
-
-// 
-hoodie.account.on('signin signup passwordreset', function() {
-  $hoodieAccountModal.modal('hide')
-});
