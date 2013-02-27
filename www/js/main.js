ae45c4f6e390b6ad029d30add4e9645abaaa6135
Gregor Martynus <gregor@martynus.net>
2013-02-27 10:33:31 +0100

    simplification

--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,4 @@
-origin = location.protocol + '//' + location.hostname
-defaultHostname = origin.replace(location.hostname, 'api.' + location.hostname)
-
-baseUrl = localStorage.getItem('baseUrl')
-
 hoodie  = new Hoodie(baseUrl)
-$('#hoodieBaseUrl').text(hoodie.baseUrl)
-$('#changeHoodieBaseUrl').click(function(event) {
-  $el = $(event.target)
-
-  localStorage.setItem('baseUrl', prompt("enter hoodie base URL", defaultHostname))
-
-  reload = function() { window.setTimeout( function() {
-    location.reload()
-  }, 3000) };
-  
-  hoodie.account.signOut()
-    .done( reload )
-    .fail( reload )
-})
 
 $hoodieAccountModal = $('#hoodieAccountModal')
 .on('shown', function() {
