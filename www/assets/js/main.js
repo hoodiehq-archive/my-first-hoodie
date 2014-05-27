44915d0f2ddd0a2348843aa2f1090e7669ad79c0
frega <fl@flink-solutions.de>
2014-05-27 13:14:21 +0200

    Fixes issue 57 - on signup duplication, inconsistent ordering, add ability to remove/close todos

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,30 +1,69 @@
+"use strict";
 // initialize Hoodie
-var hoodie  = new Hoodie()
+var hoodie  = new Hoodie();
+
+// Todos Collection/View
+function Todos($element) {
+  var collection = [];
+  var $el = $element;
+
+  $el.on('click', 'input[type=checkbox]', function() {
+    hoodie.store.remove('todo', $(this).data('id'));
+    return false;
+  });
+
+  function paint() {
+    $el.html('');
+    collection.sort(function(a, b) {
+      return ( a.createdAt > b.createdAt ) ? 1 : -1;
+    });
+    for (var i = 0, len = collection.length; i<len; i++) {
+      $el.append('<li><input data-id="' + collection[i].id + '" type="checkbox"> '+collection[i].title+'</li>');
+    }
+  }
+
+  this.add = function(todo) {
+    collection.push(todo);
+    paint();
+  };
+
+  this.remove = function(todo) {
+    for (var i = 0, len = collection.length; i < len; i++) {
+      if (collection[i].id === todo.id) {
+        collection.splice(i, 1);
+        break;
+      }
+    }
+    paint();
+  };
+
+  this.clear = function() {
+    collection = [];
+    paint();
+  };
+}
+
+// Instantiate Todos collection & view.
+var todos = new Todos($('#todolist'));
 
 // initial load of all todo items from the store
-hoodie.store.findAll('todo').then( function(todos) {
-  todos.sort( sortByCreatedAt ).forEach( addTodo )
-})
+hoodie.store.findAll('todo').then(function(allTodos) {
+  allTodos.forEach(todos.add);
+});
 
 // when a new todo gets stored, add it to the UI
-hoodie.store.on('add:todo', addTodo)
-// clear todo list when the get wiped from store
-hoodie.account.on('signout', clearTodos)
+hoodie.store.on('add:todo', todos.add);
+hoodie.store.on('remove:todo', todos.remove);
+// clear todos when user logs out,
+hoodie.account.on('signout', todos.clear);
+// clear todos when user signsup, because post signup there's a full sync/reload
+hoodie.account.on('signup', todos.clear);
+
 
 // handle creating a new task
 $('#todoinput').on('keypress', function(event) {
-  if (event.keyCode == 13) { // ENTER
+  if (event.keyCode === 13) { // ENTER
     hoodie.store.add('todo', {title: event.target.value});
     event.target.value = '';
   }
-})
-
-function addTodo( todo ) { 
-  $('#todolist').append('<li>'+todo.title+'</li>');
-}
-function clearTodos() {
-  $('#todolist').html('');
-}
-function sortByCreatedAt(a, b) { 
-  return a.createdAt > b.createdAt
-}
+});
